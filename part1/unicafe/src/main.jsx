dbcf80d73bea8263260f4f7f6b41e49c40fade92
VRVG1 <viravago@gmail.com>
2023-03-24 16:51:25 -0600

    Course information, step 2

--- part1/unicafe/src/main.jsx
+++ part1/unicafe/src/main.jsx
@@ -10,6 +10,18 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 /**
+ * Refactor Content Component in to a Part component
+ * @param {*} props
+ * @returns JSX component
+ **/
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+/**
  * App content
  * @param {*} props
  * @returns
@@ -17,15 +29,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   );
 };
