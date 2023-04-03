d592098c087fbb8a1b1f1aca7cfb32e87925dfa8
VRVG1 <viravago@gmail.com>
2023-04-03 11:36:56 -0600

    2.2 courseinfo step7

--- part2/courseinfo/src/components/couse.jsx
+++ part2/courseinfo/src/components/couse.jsx
@@ -52,6 +52,7 @@ const Course = ({course}) => {
         <div>
           <Header course={course.name} />
           <Content parts={course.parts} />
+          <Total parts={course.parts} />
         </div>
       );
 }
