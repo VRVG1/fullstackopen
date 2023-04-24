c5d4f159f9f70265bc57b847513042bb165e8a98
VRVG1 <viravago@gmail.com>
2023-04-24 09:45:12 -0600

    2.9 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 import Persons from "./components/Persons"
+import Input from "./components/Input"
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
   ])
+  const [personstoShow, setPersonstoShow] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+  ])
   const [newName, setNewName] = useState({
     name: "",
     number: "",
+  })
+
+  const [filterBy, setFilterBy] = useState({
+    name: "",
   })
 
   const addPerson = (event) => {
@@ -16,6 +24,7 @@ const App = () => {
     const nameAlready = persons.filter((person) => person.name === newName.name)
     if (nameAlready.length === 0) {
       setPersons(persons.concat(newName))
+      setPersonstoShow(persons.concat(newName))
     } else {
       alert(`${newName.name} is already added to phonebook`)
     }
@@ -27,18 +36,40 @@ const App = () => {
     setNewName({ ...newName, [name]: value })
   }
 
+  const handleOnChangeFilter = (event) => {
+    const { value } = event.target
+    setFilterBy({ name: value })
+    console.log(persons)
+    setPersonstoShow(
+      persons.filter((person) => person.name.toLowerCase().includes(value))
+    )
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <Input
+          label={"Filter shown with"}
+          name={"filter"}
+          onChange={handleOnChangeFilter}
+          value={filterBy.name}
+        />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:{" "}
-          <input name={"name"} onChange={handleOnChange} value={newName.name} />
+          <Input
+            label={"name"}
+            name="name"
+            onChange={handleOnChange}
+            value={newName.name}
+          />
         </div>
         <div>
-          number:{" "}
-          <input
-            name={"number"}
+          <Input
+            label={"number"}
+            name="number"
             onChange={handleOnChange}
             value={newName.number}
           />
@@ -48,7 +79,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons personsToShow={persons} />
+      <Persons personsToShow={personstoShow} />
     </div>
   )
 }
