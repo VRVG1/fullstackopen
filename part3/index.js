bfaab94b2ca44bf7bd96e7e90e47ee1124fc0e2b
VRVG1 <viravago@gmail.com>
2023-05-20 18:55:59 -0600

    3.13 backend phonebook step13

--- part3/index.js
+++ part3/index.js
@@ -2,40 +2,21 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
-const cors = require('cors')
+const cors = require("cors")
+const Contact = require("./phonebook")
+require("dotenv").config()
 
 // Forntend
-app.use(express.static('build'))
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-]
+app.use(express.static("build"))
 
 /**
-* Custom token for morgan
-*/
-morgan.token('body', (request) => JSON.stringify(request.body))
+ * Custom token for morgan
+ */
+morgan.token("body", (request) => JSON.stringify(request.body))
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+)
 app.use(cors())
 const generateId = () => {
   return Math.floor(Math.random() * 1000000) + 1
@@ -45,7 +26,10 @@ const generateId = () => {
  * Retrieve all persons from the server and return them as a JSON response
  */
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  //response.json(persons)
+  Contact.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 /**
@@ -59,9 +43,12 @@ app.get("/info", (request, response) => {
 })
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.filter((person) => person.id === id)
-  person ? response.json(person) : response.status(404).end()
+  // const id = Number(request.params.id)
+  // const person = persons.filter((person) => person.id === id)
+  // person ? response.json(person) : response.status(404).end()
+  Contact.findById(request.params.id).then((contact) => {
+    response.json(contact)
+  })
 })
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -99,10 +86,8 @@ app.post("/api/persons", bodyParser.json(), (request, response) => {
 
   response.json(person)
 })
-/**
- * Set the server to listen on port 3001 and log a message to the console when it starts running
- */
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
