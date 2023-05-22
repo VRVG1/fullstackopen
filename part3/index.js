c73bb4373d4d9e2eb326a02e56f4a227069f5ff2
VRVG1 <viravago@gmail.com>
2023-05-22 10:51:37 -0600

    3.22 Lint configuration

--- part3/index.js
+++ part3/index.js
@@ -1,23 +1,23 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
-const morgan = require("morgan")
-const cors = require("cors")
-const Contact = require("./models/phonebook")
-require("dotenv").config()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const Contact = require("./models/phonebook");
+require("dotenv").config();
 
-app.use(express.static("build"))
+app.use(express.static("build"));
 
 /**
  * Custom token for morgan
  */
-morgan.token("body", (request) => JSON.stringify(request.body))
+morgan.token("body", (request) => JSON.stringify(request.body));
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
-)
+);
 
-app.use(cors())
+app.use(cors());
 
 /**
  * Retrieve all persons from the server and return them as a JSON response
@@ -25,104 +25,107 @@ app.use(cors())
 app.get("/api/persons", (request, response) => {
   //response.json(persons)
   Contact.find({}).then((persons) => {
-    response.json(persons)
-  })
-})
+    response.json(persons);
+  });
+});
 
 /**
  * Return information about the phonebook, including the number of people and the current date
  */
-app.get("/info", (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people.</p>
-    <p>${new Date()}</p>
-  `)
-})
+app.get("/info", (request, response, next) => {
+  Contact.find({}).then((contact) => {
+    response.send(
+      `<p>Phonebook has info for ${
+        contact.length
+      } people</p><p>${new Date()}</p>`
+    );
+  }).catch((error) => next(error));
+});
 
 app.get("/api/persons/:id", (request, response, next) => {
   Contact.findById(request.params.id)
     .then((contact) => {
       if (contact) {
-        response.json(contact)
+        response.json(contact);
       } else {
-        response.status(404).end()
+        response.status(404).end();
       }
     })
     .catch((error) => {
-      next(error)
-    })
-})
+      next(error);
+    });
+});
 
 app.delete("/api/persons/:id", (request, response, next) => {
   Contact.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end()
+    .then(() => {
+      response.status(204).end();
     })
     .catch((error) => {
-      next(error)
-    })
-})
+      next(error);
+    });
+});
 
 app.post("/api/persons", bodyParser.json(), (request, response, next) => {
-  const body = request.body
+  const body = request.body;
 
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: "name and number are required",
-    })
+    });
   }
 
   const contact = new Contact({
     name: body.name,
     number: body.number,
-  })
+  });
 
   contact
     .save()
     .then((savedContact) => {
-      response.json(savedContact)
+      response.json(savedContact);
     })
     .catch((error) => {
-      next(error)
-    })
-})
+      next(error);
+    });
+});
 
 app.put("/api/persons/:id", bodyParser.json(), (request, response, next) => {
-  const body = request.body
+  const body = request.body;
 
   const contact = {
     name: body.name,
     number: body.number,
-  }
+  };
 
   Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
     .then((result) => {
-      response.json(result)
+      response.json(result);
     })
     .catch((error) => {
-      next(error)
-    })
-})
+      next(error);
+    });
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const errorhandler = (error, request, response, next) => {
-  console.log(error.message)
+  console.log(error.message);
   if (error.name === "CastError") {
-    return response.status(404).send({ error: "malformatted id" })
+    return response.status(404).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message });
   }
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorhandler)
+app.use(errorhandler);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
