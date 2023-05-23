d8edacfdd50a3344998d25179c4a60659893d8ea
VRVG1 <viravago@gmail.com>
2023-05-22 18:35:59 -0600

    4.4 helper functions and unit tests, step2

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,8 +1,26 @@
-const listhelper = require('../utils/list_helper')
+const listHelper = require('../utils/list_helper')
+const blogsList = require('./blog_list')
 
 test('dummy returns one', () => {
   const blogs = []
 
-  const result = listhelper.dummy(blogs)
+  const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+});
+
+describe('Total likes', () => {
+  test('when list has zweo blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogsList.zeroBlogs)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogsList.oneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list has many blogs, equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogsList.manyBlogs)
+    expect(result).toBe(36)
+  })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, post) => sum + post.likes, 0)
+}
 module.exports = {
   dummy,
+  totalLikes,
 }
