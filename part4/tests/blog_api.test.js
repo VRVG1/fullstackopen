c719a5200483d5b8feaa3fdfd8492982b6593348
VRVG1 <viravago@gmail.com>
2023-05-24 18:59:18 -0600

    Blog list tests, step2

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -17,6 +17,17 @@ test('blogs are returned as json', async () => {
 		.expect('Content-Type', /application\/json/)
 })
 
+// second test 
+test('blogs have id property named id instead of id', async () => {
+	const response = await api.get('/api/blogs')
+	const ids = response.body.map((blog) => blog.id)
+    
+	ids.forEach((id) => {
+		console.log(id)
+		expect(id).toBeDefined()
+	})
+})
+
 afterAll(async () => {
 	await mongoose.connection.close()
 })
