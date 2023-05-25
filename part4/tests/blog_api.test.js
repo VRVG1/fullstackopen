f7f98db5a626b16aa62051194f317b7efd1d714a
VRVG1 <viravago@gmail.com>
2023-05-24 19:24:45 -0600

    4.11 Blog list tests, step4

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -9,7 +9,7 @@ beforeEach(async () => {
 	await Blog.deleteMany({})
 	await Blog.insertMany(helper.manyBlogs)
 })
-// First test
+// test 1
 test('blogs are returned as json', async () => {
 	await api
 		.get('/api/blogs')
@@ -17,7 +17,7 @@ test('blogs are returned as json', async () => {
 		.expect('Content-Type', /application\/json/)
 })
 
-// second test 
+// test 2
 test('blogs have id property named id instead of id', async () => {
 	const response = await api.get('/api/blogs')
 	const ids = response.body.map((blog) => blog.id)
@@ -27,7 +27,7 @@ test('blogs have id property named id instead of id', async () => {
 	})
 })
 
-// third test
+// test 3
 test('a valid blog can be created', async () => {
 	const newBlog = {
 		title: 'The Great Gatsby',
@@ -49,6 +49,26 @@ test('a valid blog can be created', async () => {
 	const titles = blogInDB.map((blog) => blog.title)
 	expect(titles).toContain('The Great Gatsby')
 })
+
+// test 4
+test('likes property defaults to 0 if missing', async () => {
+	const newBlog = {
+		title: 'The Great Gatsby',
+		author: 'F. Scott Fitzgerald',
+		url: 'https://example.com/blog/the-great-gatsby',
+	}
+    
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+	
+	const blogInDB = await helper.blogsInDb()
+	expect(blogInDB).toHaveLength((helper.manyBlogs.length + 1))
+	expect(blogInDB[blogInDB.length - 1].likes).toBe(0)
+})
+
 
 afterAll(async () => {
 	await mongoose.connection.close()
