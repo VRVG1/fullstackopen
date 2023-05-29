4403eaa8cbe8eddabd4f695d56e316c03a5cddfa
VRVG1 <viravago@gmail.com>
2023-05-28 18:25:23 -0600

    4.16: bloglist expansion, step4

--- part4/controllers/user.js
+++ part4/controllers/user.js
@@ -3,30 +3,40 @@ const usersRouter = require('express').Router()
 const User = require('../models/users')
 
 usersRouter.get('/', async (request, response) => {
-	const users = await User.find({}).populate('blogs', {
-		title: 1,
-		author: 1,
-		url: 1,
-	})
-	response.json(users)
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+  })
+  response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
-	const { username, name, password } = request.body
+  const { username, name, password } = request.body
 
-	
-    
-	const saltRounds = 10
-	const passwordHash = await bcrypt.hash(password, saltRounds)
+  if (!(username && password)) {
+    return response.status(400).json({
+      error: 'username and password are required',
+    })
+  }
 
-	const user = new User({
-		username,
-		name,
-		passwordHash
-	})
+  if (username.length < 4 || password.length < 4) {
+    return response.status(400).json({
+      error: 'username and password must be at least 3 characters long'
+    })
+  }
 
-	const savedUser = await user.save()
-	response.status(201).json(savedUser)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
 })
 
 // usersRouter.delete('/:id', async (request, response) => {
